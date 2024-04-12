33f510f3dae90ad8aa6189e8f6f97a860c908360
retzam <dretzam@gmail.com>
2024-04-12 12:22:56 +0100

    Update knn features

--- features/knn/components/title.tsx
+++ features/knn/components/title.tsx
@@ -9,30 +9,27 @@ export default function SupervisedLearningPageTitle() {
         <RocketIcon className="h-4 w-4" />
         <AlertTitle>Supervised Learning Models</AlertTitle>
         <AlertDescription>
-          <p>This page aims to illustrate supervised learning models.</p>
-          <p>
-            This shows the precision, recall and accuracy of the model. <br />
-            It also shows prediction results for each model.
-          </p>
-          <p className="mt-4">
-            We have a test as well to show how the model works in prediction.{' '}
-            <br />
-            For this test, we used a dataset of 90,000+ cars data from 1970 to
-            2024.
-            <br />
-            <br />
-            You will input a car&apos;s information like manufacturing price,
-            year, mileage, mpg engine size etc. and each model would predict the
-            manufacturer of the car. <br />
-            We got this dataset from Kaggle you can view and download{' '}
-            <a
-              className="font-bold text-blue-500"
-              href="https://www.kaggle.com/datasets/meruvulikith/90000-cars-data-from-1970-to-2024"
-            >
-              here
-            </a>
-            .
-          </p>
+          <div className="prose prose-sm prose-invert max-w-none">
+            <p className="mt-4">
+              For this demo project, we used a dataset of 90,000+ cars data from
+              1970 to 2024.
+            </p>
+            <p>
+              You will input a car&apos;s information like manufacturing price,
+              year, mileage, mpg engine size etc, and each model would predict
+              the manufacturer of the car.
+            </p>
+            <p>
+              We got this dataset from Kaggle you can view and download{' '}
+              <a
+                className="font-bold text-blue-500"
+                href="https://www.kaggle.com/datasets/meruvulikith/90000-cars-data-from-1970-to-2024"
+              >
+                here
+              </a>
+              .
+            </p>
+          </div>
         </AlertDescription>
       </Alert>
     </div>
