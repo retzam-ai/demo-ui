3614aa657b9d8b472c9c3ea0a379cd9ce138ac65
retzam <dretzam@gmail.com>
2024-04-13 18:38:40 +0100

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   future: {
     hoverOnlyWhenSupported: true,
