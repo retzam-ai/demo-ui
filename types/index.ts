5f6faac8ec27b9219c739ceb22c2bb63c1f474a3
retzam <dretzam@gmail.com>
2024-04-19 00:11:07 +0100

    Update types add injury schema

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,7 @@
 import { LucideIcon } from 'lucide-react';
 import z from 'zod';
 
-export const supervisedLearningPredictionSchema = z.object({
+export const carsPredictionSchema = z.object({
   year: z.coerce.number().min(1970).max(2024),
   price: z.coerce.number().min(450).max(160000),
   transmission: z.string(),
@@ -10,6 +10,15 @@ export const supervisedLearningPredictionSchema = z.object({
   tax: z.coerce.number().min(0).max(580),
   mpg: z.coerce.number().min(0.43).max(471),
   engineSize: z.coerce.number().min(0.6).max(6.6),
+});
+
+export const injuryPredictionSchema = z.object({
+  age: z.coerce.number().min(1).max(100),
+  weight: z.coerce.number().min(1).max(200),
+  height: z.coerce.number().min(1).max(300),
+  previousInjuries: z.coerce.number().min(0).max(1),
+  trainingIntensity: z.coerce.number().min(0).max(1),
+  recoveryTime: z.coerce.number().min(1).max(6),
 });
 
 export type SidebarLink = {
@@ -21,13 +30,10 @@ export type SidebarLink = {
   variant: 'default' | 'ghost';
 };
 
-export type ModelPredictionType = {
-  prediction: string;
+export type SupervisedLearningClassificationPredictionType = {
+  knn: string;
+  naiveBayes: string;
   isLoading: boolean;
   error: string;
-};
-
-export type SupervisedLearningPredictionModelsType = {
-  knn: ModelPredictionType;
   triggered: boolean;
 };
